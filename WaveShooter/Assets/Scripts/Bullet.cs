8c12327e7d7f25e6a7c33fa3b6de90825fb0753f
Dylano2810 <bakmeijerdylano@gmail.com>
2026-04-15 14:14:12 +0200

    base turret stuff

---
+++ WaveShooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    [Header("Attributes")]
+    [SerializeField] private float bulletSpeed = 9f;
+    [SerializeField] private int bulletDamage = 1;
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!target) return;
+
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.linearVelocity = direction * bulletSpeed;
+    }
+ 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+       // other.gameObject.GetComponent<Health>().TakeDamage(bulletDamage);
+        Destroy(gameObject);
+    }
+}
--- WaveShooter/Assets/Scripts/Gun.cs
+++ WaveShooter/Assets/Scripts/Gun.cs
@@ -2,15 +2,70 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Header("References")]
+    [SerializeField] private Transform Rotationpoint;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform gunpoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attributes")]
+    [SerializeField] private float targetInRange = 4f;
+    [SerializeField] private float rotationspeed = 5f;
+    [SerializeField] private float bulletPerSecond = 1f;
+    [SerializeField] private Transform target;
+    [SerializeField] private float timeUntilFire;
+
+    private void Update()
     {
-        
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+            if (timeUntilFire >= 1f / bulletPerSecond)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Shoot()
     {
-        
+        GameObject bulletB = Instantiate(bulletPrefab, gunpoint.position, Quaternion.identity);
+        Bullet bulletscript = bulletB.GetComponent<Bullet>();
+        bulletscript.SetTarget(target);
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetInRange, (Vector2)
+            transform.position, 0f, enemyMask);
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetInRange;
+    }
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        Rotationpoint.rotation = Quaternion.RotateTowards(Rotationpoint.rotation, targetRotation, rotationspeed * Time.deltaTime);
     }
 }
